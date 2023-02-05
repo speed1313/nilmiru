e582000a0a010357ea5daad40d85521880f7f68e
speed1313 <speedtry13@icloud.com>
2023-02-05 18:32:28 +0900

    refactoring

--- nilmiru.go
+++ nilmiru.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-const doc = "nilmiru is a golang linter which checks nil check leakage in function."
+const doc = "nilmiru is a static analysis tool that detects nil check leakage in function"
 
 // Analyzer is ...
 var Analyzer = &analysis.Analyzer{
@@ -25,43 +25,36 @@ func run(pass *analysis.Pass) (any, error) {
 		return nil, nil
 	}
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	pointerVarList := []types.Object{}
 	varIsCheckedTable := map[types.Object]bool{}
 	inspect.Preorder(nil, func(n ast.Node) {
 		switch n := n.(type) {
 		case *ast.FuncDecl:
-			pointerVarList = checkFuncDecl(pass, n, pointerVarList)
+			checkFuncDecl(pass, n, varIsCheckedTable)
 		case *ast.Ident:
-			checkIdent(pass, n, pointerVarList, varIsCheckedTable)
-
+			checkIdent(pass, n, varIsCheckedTable)
 		case *ast.IfStmt:
-			checkIfStmt(pass, n, pointerVarList, varIsCheckedTable)
+			checkIfStmt(pass, n, varIsCheckedTable)
 		}
-
 	})
-
 	return nil, nil
 }
 
 // store arguments which require nil check
-func checkFuncDecl(pass *analysis.Pass, funcDecl *ast.FuncDecl, pointerVarList []types.Object) []types.Object {
+func checkFuncDecl(pass *analysis.Pass, funcDecl *ast.FuncDecl, varIsCheckedTable map[types.Object]bool) {
 	for _, field := range funcDecl.Type.Params.List {
 		for _, name := range field.Names {
 			obj := pass.TypesInfo.ObjectOf(name)
 			switch obj.Type().(type) {
-			case *types.Pointer:
-				pointerVarList = append(pointerVarList, pass.TypesInfo.Defs[name])
-			case *types.Slice:
-				pointerVarList = append(pointerVarList, pass.TypesInfo.Defs[name])
+			case *types.Pointer, *types.Slice:
+				varIsCheckedTable[pass.TypesInfo.Defs[name]] = false
 			}
 		}
 	}
-	return pointerVarList
 }
 
 // check if the variable is used after nil check
-func checkIdent(pass *analysis.Pass, ident *ast.Ident, pointerVarList []types.Object, varIsCheckedTable map[types.Object]bool) {
-	for _, obj := range pointerVarList {
+func checkIdent(pass *analysis.Pass, ident *ast.Ident, varIsCheckedTable map[types.Object]bool) {
+	for obj := range varIsCheckedTable {
 		if ident_obj := pass.TypesInfo.Uses[ident]; ident_obj != nil {
 			if ident_obj == obj {
 				if !varIsCheckedTable[obj] {
@@ -73,23 +66,26 @@ func checkIdent(pass *analysis.Pass, ident *ast.Ident, pointerVarList []types.Ob
 }
 
 // update nil check table
-func checkIfStmt(pass *analysis.Pass, ifStmt *ast.IfStmt, pointerVarList []types.Object, varIsCheckedTable map[types.Object]bool) {
+func checkIfStmt(pass *analysis.Pass, ifStmt *ast.IfStmt, varIsCheckedTable map[types.Object]bool) {
 	switch ifStmt := ifStmt.Cond.(type) {
 	case *ast.BinaryExpr:
-		switch ifStmt.X.(type) {
+		switch X := ifStmt.X.(type) {
 		case *ast.Ident:
 			if types.Identical(pass.TypesInfo.TypeOf(ifStmt.Y), types.Typ[types.UntypedNil]) {
-				obj := pass.TypesInfo.ObjectOf(ifStmt.X.(*ast.Ident))
+				obj := pass.TypesInfo.ObjectOf(X)
 				varIsCheckedTable[obj] = true
 			}
 		case *ast.CallExpr:
-			if ifStmt.X.(*ast.CallExpr).Fun.(*ast.Ident).Name == "len" {
-				if types.Identical(pass.TypesInfo.TypeOf(ifStmt.Y), types.Typ[types.Int]) {
-					obj := pass.TypesInfo.ObjectOf(ifStmt.X.(*ast.CallExpr).Args[0].(*ast.Ident))
-					varIsCheckedTable[obj] = true
+			if name, ok := X.Fun.(*ast.Ident); ok {
+				if name.Name == "len" {
+					if types.Identical(pass.TypesInfo.TypeOf(ifStmt.Y), types.Typ[types.Int]) {
+						if ident, ok := X.Args[0].(*ast.Ident); ok {
+							obj := pass.TypesInfo.ObjectOf(ident)
+							varIsCheckedTable[obj] = true
+						}
+					}
 				}
 			}
-
 		}
 	}
 }
